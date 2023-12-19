a57262ebc9f49af0dde9edbd939b8f9811bf2e60
gitrosso <rossano.angius.com@gmail.com>
2023-12-19 22:46:01 +0100

    Pulizia codice

--- src/main/java/laoreProjects/IRTiBE/authentication/configuration/AuthenticationConfiguration.java
+++ src/main/java/laoreProjects/IRTiBE/authentication/configuration/AuthenticationConfiguration.java
@@ -4,31 +4,22 @@ import laoreProjects.IRTiBE.authentication.jwtElements.JWTFilterAuthentication;
 import laoreProjects.IRTiBE.authentication.jwtElements.JWTUserDetailsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -51,7 +42,7 @@ public class AuthenticationConfiguration {
 
                 .authorizeHttpRequests((authorize) -> authorize
 
-                        .requestMatchers("/authentication/login", "/authentication/changePasswordAccount").permitAll()
+                        .requestMatchers("/authentication/login").permitAll()
                         .anyRequest().authenticated()
                         //.anyRequest().permitAll()
 
@@ -66,20 +57,6 @@ public class AuthenticationConfiguration {
                 .build();
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        //Make the below setting as * to allow connection from any hos
-//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8210"));
-//        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.setAllowedHeaders(List.of("*"));
-//        corsConfiguration.setMaxAge(3600L);
-//        corsConfiguration.addExposedHeader("Authorization");
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/dds/**", corsConfiguration);
-//        return source;
-//    }
     @Bean
         // AuthenticationProvider for user and password authentication
     AuthenticationProvider authenticationProvider(){
